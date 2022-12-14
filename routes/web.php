ceb1c8eeba01b4da0f2b56594b1265162f882542
gioanbatan <giovannibassani@hotmail.com>
2022-12-14 15:23:30 +0100

    Add: Hello World via route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        "test" => "Hello Word"
+    ];
+    return view('home', $data);
+})->name('home');
